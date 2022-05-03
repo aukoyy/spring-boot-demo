b0dc93ee8e7348abbab4d988e9b0976b1a269949
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2022-05-03 09:15:01 +0200

    add hello world getter

--- src/main/kotlin/com/example/demo/SpringDemoApplication.kt
+++ src/main/kotlin/com/example/demo/SpringDemoApplication.kt
@@ -2,10 +2,24 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
 
 @SpringBootApplication
 class SpringDemoApplication
 
 fun main(args: Array<String>) {
 	runApplication<SpringDemoApplication>(*args)
+}
+
+@Controller
+class HtmlController {
+
+	@GetMapping("/hello")
+	fun helloKotlin(): String {
+		return "hello world"
+	}
+
 }
